3b5f4ac4c958966bff53164d4738fb7edfea1560
regmarmcem <tknktknktknktknktknk@gmail.com>
2024-06-03 21:36:19 +0900

    [feature] return Random status

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/labstack/echo/v4"
 )
@@ -9,7 +10,14 @@ import (
 func main() {
 	e := echo.New()
 	e.GET("/status", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Success")
+		status := "InProgress"
+		now := time.Now()
+		lastDigit := now.Second() % 10
+		if lastDigit == 0 {
+			status = "Success"
+		}
+
+		return c.String(http.StatusOK, status)
 	})
 	e.Logger.Panic(e.Start(":1323"))
 }
